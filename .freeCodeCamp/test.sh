469a685218e7000c61091db046b79a778a7f6bcc
Razanajatovo Heritiana <razanajatovo.simeone@gmail.com>
2026-07-07 06:08:43 -0400

    stage 15

---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+
+echo $QUESTION1
+
+read NAME
+echo $QUESTION2
+read LOCATION
+echo Hello $NAME.
+echo $LOCATION
---
+++ questionnaire.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+
+echo -e "\n~~ Questionnaire ~~\n"
+
+echo $QUESTION1
+
+read NAME
+echo $QUESTION2
+read LOCATION
+echo Hello $NAME from $LOCATION.
