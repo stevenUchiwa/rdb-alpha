3d70e7b4e6f6cf031703e149661739daef5644cd
Razanajatovo Heritiana <razanajatovo.simeone@gmail.com>
2026-07-07 06:11:29 -0400

    Save progress: 340.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
+echo -e "\n~~ Questionnaire ~~\n"
 
 echo $QUESTION1
 
 read NAME
 echo $QUESTION2
 read LOCATION
-echo Hello $NAME.
-echo $LOCATION
+echo Hello $NAME from $LOCATION.
+echo $QUESTION3
--- questionnaire.sh
+++ questionnaire.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 
 echo -e "\n~~ Questionnaire ~~\n"
 
