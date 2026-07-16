2426be572fc0aa4be442ab60d1eeda2c4b9f8b63
Razanajatovo Heritiana <razanajatovo.simeone@gmail.com>
2026-07-16 14:38:47 -0400

    Save progress: 556.1

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#Program that counts down to zero from a given argument
-# if [[ $1 -le 5 ]]
-# then
-#   echo true
-# else
-#   echo false
-# fi
+
+# Program that counts down to zero from a given argument
+
+if [[ $1 -le 5 ]]
+then
+  echo true
+else
+  echo false
+fi
--- questionnaire.sh
+++ questionnaire.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# QUESTION1="What's your name?"
-# QUESTION2="Where are you from?"
-# QUESTION3="What's your favorite coding website?"
 
-# echo -e "\n~~ Questionnaire ~~\n"
+echo -e "\n~~ Questionnaire ~~\n"
 
-# echo $QUESTION1
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 
-# read NAME
-# echo $QUESTION2
-# read LOCATION
-# echo $QUESTION3
-# read WEBSITE
-# echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
-echo $?
+echo $QUESTION1
+read NAME
+
+echo $QUESTION2
+read LOCATION
+
+echo $QUESTION3
+read WEBSITE
+
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
