8cf5a6f080cb305b6cdb453767bbbba83902107f
Razanajatovo Heritiana <razanajatovo.simeone@gmail.com>
2026-07-17 09:17:20 -0400

    Save progress: 730.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-QUESTION1="What's your name?"
-QUESTION2="Where are you from?"
-QUESTION3="What's your favorite coding website?"
 
-echo -e "\n~~ Questionnaire ~~\n"
+# Program that counts down to zero from a given argument
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
+then
+: '
+  for (( i = $1; i >= 0; i--))
+  do
+    echo $i
+    sleep 1
+  done
+  '
+  I=$1
+else
+  echo Include a positive integer as the first argument.
+fi
 
-echo $QUESTION1
-
-read NAME
-echo $QUESTION2
-read LOCATION
-echo $QUESTION3
-read WEBSITE
-echo Hello $NAME from $LOCATION.
-echo $WEBSITE
+echo $I
