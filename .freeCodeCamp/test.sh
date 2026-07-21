345518e31dc1e32d835cdfab5c6f0f3886ad7299
Razanajatovo Heritiana <razanajatovo.simeone@gmail.com>
2026-07-21 07:31:58 -0400

    Save progress: 1490.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,32 +1,7 @@
 #! /bin/bash
-# Program to tell a persons fortune
+# Program to run my other four programs
 
-echo -e "\n~~ Fortune Teller ~~\n"
-
-RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( RANDOM % 6 ))
-
-GET_FORTUNE () {
-  if [[ ! $1 ]]
-  then
-    echo "Ask a yes or no question:"
-  else
-    echo "Try again. Make sure it ends with a question mark:"
-  fi
-
-  read QUESTION
-}
-
-
-
-echo ${RESPONSES[$N]}
-
-
-
-until [[ $QUESTION =~ \?$ ]]
-do
-  GET_FORTUNE
-done
-
-
-GET_FORTUNE again
+./questionnaire.sh
+./countdown.sh a
+./bingo.sh
+./fortune.sh
